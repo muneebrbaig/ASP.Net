c8f0bab423f206a808f6a04209d6737cd46bead0
MUNEEB-UR-REHMAN BAIG <muneebrbaig@users.noreply.github.com>
2016-12-21 21:34:42 -0800

    Updated with Data

--- OLD/AjaxPageMethods/AjaxPageMethods/TestService.asmx.cs
+++ OLD/AjaxPageMethods/AjaxPageMethods/TestService.asmx.cs
@@ -1,47 +1,101 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Services;
 
-namespace AjaxPageMethods
+namespace AjaxPageMethod
 {
-  /// <summary>
-  /// Summary description for TestService
-  /// </summary>
-  [WebService(Namespace = "http://tempuri.org/")]
-  [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
-  [System.ComponentModel.ToolboxItem(false)]
-  // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
-  [System.Web.Script.Services.ScriptService]
-  public class TestService : System.Web.Services.WebService
-  {
-
-    [WebMethod]
-    public string HelloWorld()
-    {
-      return "Hello World";
-    }
-
-    [WebMethod]
-    public string GetServerTime()
-    {
-      return String.Format("The server time is {0}.",
-                DateTime.Now);
-    }
-
     /// <summary>
-    /// Exposing Web Services to Client Script
-    ///  https://goo.gl/6s5roc
+    /// Summary description for LearningService
     /// </summary>
-    /// <param name="input"></param>
-    /// <returns></returns>
-    [WebMethod]
-    public string EchoInput(string input)
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    [System.Web.Script.Services.ScriptService]
+    public class LearningService : System.Web.Services.WebService
     {
-      // Method code goes here.
-      return input + "<br>" + DateTime.Now;
-    }
 
-  }
+        [WebMethod]
+        public string HelloWorld()
+        {
+            List<string> lstData = GetProductsList();
+
+            string output = "<ul>";
+            foreach (string s in lstData)
+            {
+                output += "<li>" + s + "</li>";
+            }
+            //for (int x = 1; x <= 10; x++)
+            //    output += "<li>" + x + "</li>";
+            output += "</ul>";
+
+            return output;
+        }
+
+        private static List<string> GetStudentList()
+        {
+            List<string> lstData = new List<string>();
+            lstData.Add("Sadaqat");
+            lstData.Add("Arsalan");
+            lstData.Add("Bilal");
+            lstData.Add("Faisal");
+            lstData.Add("Owais");
+            lstData.Add("Qasim");
+            lstData.Add("Abdul Rehman");
+            return lstData;
+        }
+
+        private static List<string> GetProductsList()
+        {
+            List<string> lstData = new List<string>();
+            lstData.Add("Tea");
+            lstData.Add("Coffe");
+            lstData.Add("Bread");
+            lstData.Add("Milk");
+            lstData.Add("Toffee");
+            lstData.Add("Syrup");
+            lstData.Add("Chocolate");
+            return lstData;
+        }
+
+        [WebMethod]
+        public string GetServerTime()
+        {
+            return DateTime.Now.ToString();
+        }
+
+        /// <summary>
+        /// Exposing Web Services to Client Script
+        ///  https://goo.gl/6s5roc
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        [WebMethod]
+        public string EchoInput(string input)
+        {
+            // Method code goes here.
+            return input + "<br>" + DateTime.Now;
+        }
+        /*
+        [WebMethod]
+        public string SearchData(string search) 
+        {
+
+            List<string> lstData = GetProductsList();
+            List<string> result = lstData.Reverse();
+            string output = "<ul>";
+            foreach (string s in result)
+            {
+                output += "<li>" + s + "</li>";
+            }
+            //for (int x = 1; x <= 10; x++)
+            //    output += "<li>" + x + "</li>";
+            output += "</ul>";
+
+            return output;        
+        }*/
+
+    }
 }
