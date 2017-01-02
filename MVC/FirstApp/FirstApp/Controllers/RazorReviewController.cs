17ed282b2299aa0e78caeacce00dcb6be16abb8c
DESKTOP-4A5S0D3\Muneeb R. Baig <muneebrbaig@gmail.com>
2017-01-02 21:22:35 +0500

    MVC First App Razor Engine

---
+++ MVC/FirstApp/FirstApp/Controllers/RazorReviewController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstApp.Controllers
+{
+    public class RazorReviewController : Controller
+    {
+        // GET: RazorReview
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+      public ActionResult Loops() { return View(); }
+    public ActionResult UIControls() { return View(); }
+  }
+}
